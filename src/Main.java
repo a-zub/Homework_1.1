5fd000a7d444f10a7b32dccd150da72ab78cd9b5
Sasha <slanecru@outlook.com>
2022-08-13 14:01:36 +0300

    competed homework 1.1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// My first homework
+//Aleksandar Zubanov
+// 2020/07/28
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Thank you my teacher for teaching me!");
+    }
+}
